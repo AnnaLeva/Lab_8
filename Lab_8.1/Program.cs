4ba049c56f865833af113d163900d4b82bf1823d
Анастасия Александровна Ковалева <309755@niuitmo.ru>
2021-12-03 19:31:55 +0300

    Добавьте файлы проекта.

---
+++ Lab_8.1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lab_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "C:\\My project";
+            string[] folders = Directory.GetDirectories(path);
+            Console.WriteLine("Folders and files:\n");
+
+            foreach (string p in folders)
+            {
+                Console.WriteLine(p);
+                string subname = p;
+                DirectoryInfo fullnameSub = new DirectoryInfo(subname);
+                foreach (var file in fullnameSub.GetFiles())
+                {
+                    Console.WriteLine(file);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab_8.2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace Lab_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "C:\\tmp\\lab_8.1.txt";
+
+            if (!File.Exists(path))
+            {
+                File.Create(path).Close();
+            }
+            using (StreamWriter info = new StreamWriter(path))
+            {
+                Random r = new Random();
+                for (int i = 0; i < 10; i++)
+                {
+                    info.WriteLine(r.Next(0, 1000));
+                }
+            }
+            int[] arraySum = new int[10];
+            int sum = 0;
+            using (StreamReader info = new StreamReader(path))
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    arraySum[i] = Convert.ToInt32(info.ReadLine());
+                    sum += arraySum[i];
+                }
+            }
+            Console.WriteLine("The sum is {0}", sum);
+            Console.ReadKey();
+        }
+    }
+}
